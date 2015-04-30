7bf43a815c6f3d2c2adc4dc6a27ea367a194c276
Joe Reeve <joe@wigwamm.com>
2015-04-30 21:57:48 +0100

    added thanks page

--- site.js
+++ site.js
@@ -1,6 +1,13 @@
 var app = require('express')();
 var Load = require('ractive-load');
 app.use(require('express').static(process.cwd() + '/public'));
+var db = require('mongojs').connect('test', ['emails']);
+
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({
+  extended: true
+}));
+
 
 app.get('/', function(req, res) {
   Load('views/index.html').then(function(Component) {
@@ -11,6 +18,11 @@ app.get('/', function(req, res) {
   }).catch(function(a, b) {
     console.log(a, b)
   });
+});
+
+app.post('/newEmail', function(req,res){
+  db.emails.insert(req.body);
+  res.redirect('http://cssdeck.com/labs/full/jffmx2pi');
 });
 
 app.listen(80);
